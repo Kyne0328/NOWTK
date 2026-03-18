fe68fadaeebf0d3be295bd9af5cc67c134acc602
Kyne0328 <kynskie13@gmail.com>
2026-03-18 13:34:31 +0800

    Add exception handling to App.xaml OnStartup to catch and display startup errors

--- NOWT/App.xaml.cs
+++ NOWT/App.xaml.cs
@@ -46,61 +46,61 @@ public partial class App : Application
 
     private static void OnDispatcherUnhandledException(
         object sender,
-        DispatcherUnhandledExceptionEventArgs e
-    )
+        DispatcherUnhandledExceptionEventArgs e)
     {
-        Constants.Log.Error(
-            "Unhandled Exception: {Message}, {Stacktrace}",
-            e.Exception.Message,
-            e.Exception.StackTrace
-        );
+        Constants.Log.Error("Unhandled Exception: {Message}, {Stacktrace}",
+            e.Exception.Message, e.Exception.StackTrace);
         e.Handled = true;
     }
 
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        try
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var update_url = "https://raw.githubusercontent.com/pwall2222/NOWT/main/NOWT/VersionInfo.xml";
 
-        var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        var update_url =
-            "https://raw.githubusercontent.com/pwall2222/NOWT/main/NOWT/VersionInfo.xml";
+            Constants.LocalAppDataPath =
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\NOWT";
+            Constants.Log = new LoggerConfiguration().MinimumLevel.Debug()
+                .WriteTo.Async(a => a.File(Constants.LocalAppDataPath + "\\logs\\log.txt",
+                    shared: true, rollingInterval: RollingInterval.Day))
+                .CreateLogger();
+            Constants.Log.Information("Application Start. Version: {Version}", version);
 
-        Constants.LocalAppDataPath =
-            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\NOWT";
-        Constants.Log = new LoggerConfiguration().MinimumLevel
-            .Debug()
-            .WriteTo.Async(
-                a =>
-                    a.File(
-                        Constants.LocalAppDataPath + "\\logs\\log.txt",
-                        shared: true,
-                        rollingInterval: RollingInterval.Day
-                    )
-            )
-            .CreateLogger();
-        Constants.Log.Information("Application Start. Version: {Version}", version);
+            CheckAndUpdateJsonAsync().ConfigureAwait(false);
 
-        CheckAndUpdateJsonAsync().ConfigureAwait(false);
+            var conventionViewFactory = new NamingConventionViewFactory();
 
-        var conventionViewFactory = new NamingConventionViewFactory();
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                    .AddTransient<HomeViewModel>()
+                    .AddTransient<InfoViewModel>()
+                    .AddTransient<MatchViewModel>()
+                    .AddTransient<SettingsViewModel>()
+                    .AddSingleton<MainViewModel>()
+                    .AddSingleton<IViewFactory>(conventionViewFactory)
+                    .BuildServiceProvider()
+            );
 
-        Ioc.Default.ConfigureServices(
-            new ServiceCollection()
-                .AddTransient<HomeViewModel>()
-                .AddTransient<InfoViewModel>()
-                .AddTransient<MatchViewModel>()
-                .AddTransient<SettingsViewModel>()
-                .AddSingleton<MainViewModel>()
-                .AddSingleton<IViewFactory>(conventionViewFactory)
-                .BuildServiceProvider()
-        );
+            AutoUpdater.ShowSkipButton = false;
+            AutoUpdater.InstalledVersion = new Version(System.Windows.Forms.Application.ProductVersion);
+            AutoUpdater.Start(update_url);
 
-        AutoUpdater.ShowSkipButton = false;
-        AutoUpdater.InstalledVersion = new Version(System.Windows.Forms.Application.ProductVersion);
-        AutoUpdater.Start(update_url);
-
-        MainWindow = new MainWindow();
-        MainWindow.Show();
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+        catch (Exception ex)
+        {
+            // Log the exception and show a message box for debugging
+            Constants.Log.Error("Startup failed: {Exception}", ex);
+            System.Windows.MessageBox.Show($"Application failed to start: {ex.Message}\n\n{ex.StackTrace}", 
+                "Startup Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            // Don't call Application.Current.Shutdown() here as it might cause issues
+            // Let the exception propagate normally after showing the message
+            throw;
+        }
     }
 
     private void Application_Exit(object sender, ExitEventArgs e)
