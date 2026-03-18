07322ef70956e063f7c29487b5be33cab74c4d35
Kyne0328 <kynskie13@gmail.com>
2026-03-18 14:44:45 +0800

    UI improvements: Fix language display, remove old developer references, add Kyne0328 branding, restructure settings to vertical scrollable cards
    Changes:
    - Updated Settings.xaml to vertical card layout in ScrollViewer for better mobile/tablet usability
    - Changed language dropdown from grid layout to proper vertical stack
    - Removed all developer references and replaced with Kyne0328 branding
    - Updated App.xaml.cs version check URL to Kyne0328/NOWTK
    - Updated Settings.xaml.cs version check URL to Kyne0328/NOWTK
    - Updated Info.xaml GitHub links to Kyne0328/NOWTK
    - Removed commented out sponsor section from Info.xaml
    - Fixed language resources to properly load and apply changes
    - Maintained all existing functionality and event handlers

--- NOWT/App.xaml.cs
+++ NOWT/App.xaml.cs
@@ -63,7 +63,7 @@ public partial class App : Application
 
         var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         var update_url =
-            "https://raw.githubusercontent.com/pwall2222/NOWT/main/NOWT/VersionInfo.xml";
+            "https://raw.githubusercontent.com/Kyne0328/NOWTK/main/NOWT/VersionInfo.xml";
 
         Constants.LocalAppDataPath =
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\NOWT";
