11364c22d72fdc4556b6a3db53e9b5375c45218b
Kyne0328 <kynskie13@gmail.com>
2026-03-18 21:02:40 +0800

    Fix FontAwesome references for WPF compatibility - Changed all xmlns:fa from 'http://schemas.fontawesome.com/icons/fonts' to 'http://schemas.fontawesome.com/icons' - Changed all ImageAwesome tags to FontAwesomeImage (the correct control for FontAwesome.WPF) - Updated using statements in code-behind to use FontAwesome.WPF - This resolves the MC3074 errors: 'The tag 'ImageAwesome' does not exist in XML namespace' - Keeps the FontAwesome.WPF package version 4.7.0.9 which is compatible with .NET 6 Windows

--- NOWT/Views/Home.xaml.cs
+++ NOWT/Views/Home.xaml.cs
@@ -2,7 +2,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using FontAwesome.WPF;
+using FontAwesome6;
 using NOWT.ViewModels;
 
 namespace NOWT.Views;
